18f58be37fae7d5eafd0702f77fd20e5ee2c8124
GAURAV KOTANGALE <gauravkotangale237@gmail.com>
2025-03-02 16:05:19 +0530

    updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy: {
-      '/api': 'http://localhost:8000',
+      '/api': 'https://messchain.onrender.com',
+      // '/api': 'http://localhost:8000',
     },
   },
   plugins: [react()],
