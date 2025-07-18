29079ec7e2757653ff0852b2cc5a590e0ca84f6d
GAURAV KOTANGALE <gauravkotangale237@gmail.com>
2025-07-18 08:19:03 +0530

    foodbyte typo  fixed

--- backend/src/utils/rewardStudent.js
+++ backend/src/utils/rewardStudent.js
@@ -3,7 +3,7 @@ import { Student } from "../models/student.model.js";
 export const rewardStudentWithFoodByte = async (rollno) => {
     const student = await Student.findOne({ rollno });
     if (student) {
-        student.foodByte = (student.foodByte || 0) + 1;
+        student.foodBytes = (student.foodBytes || 0) + 1;
         await student.save();
     }
 };
